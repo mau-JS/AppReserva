1c853a3ea5d3333ac3f04785a7ab84400014faa5
Mauricio Juárez <mau@MacBook-Air-de-Mauricio-4.local>
2022-09-23 12:07:14 -0500

    Se agregaron tab bars y una pantalla de Inicio

---
+++ appReserva/Ajustes/AjustesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AjustesViewController.swift
+//  appReserva
+//
+//  Created by Mauricio Juárez on 19/09/22.
+//
+
+import UIKit
+
+class AjustesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ appReserva/Cuenta/CuentaViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CuentaViewController.swift
+//  appReserva
+//
+//  Created by Mauricio Juárez on 19/09/22.
+//
+
+import UIKit
+
+class CuentaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+
+}
