aa1a8fd7200108457be60b247192cd2f1eeabc22
Mauricio Juárez <mau@MacBook-Air-de-Mauricio-4.local>
2022-09-27 11:52:39 -0500

    Agregando cambios de pantalla modales

--- appReserva/Reserva/AddEditReservaTableViewController.swift
+++ appReserva/Reserva/AddEditReservaTableViewController.swift
@@ -1,24 +1,26 @@
 //
-//  EmojiTableViewController.swift
+//  AddEditReservaTableViewController.swift
 //  appReserva
 //
-//  Created by Mauricio Juárez on 23/09/22.
+//  Created by Mauricio Juárez on 27/09/22.
 //
 
 import UIKit
 
-class ReservaTableViewController: UITableViewController {
-    var reservas: [Reservas] = [
-        Reservas(dia: 10, mes: 10, anio: 10,diaFinal: 10, mesFinal: 10, anioFinal: 10, titulo: "A-102", tipo: "dsd", description: "Esta es una descripción")
-        //Reservas(dia: 5, mes: 10, anio: 2021, titulo: "happiness"),
-        //Reservas(dia: 7, mes: 10, anio: 2022, titulo: "marte"),
-        //Reservas(dia: 5, mes: 10, anio: 1994, titulo: "retiroA"),
-        //Reservas(dia: 5, mes: 10, anio: 1996, titulo: "Mapas"),
-        //Reservas(dia: 5, mes: 10, anio: 2010, titulo: "hola")
-    ]
+class AddEditReservaTableViewController: UITableViewController {
+    var reservas: Reservas?
+    
+    init?(coder: NSCoder, reservas: Reservas?){
+        self.reservas = reservas
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.leftBarButtonItem = editButtonItem
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -27,41 +29,17 @@ class ReservaTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-    
-    //Es para dar la opción de borrar celdas
-    
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-        let moverReserva = reservas.remove(at: fromIndexPath.row)
-        reservas.insert(moverReserva, at: to.row)
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tableView.reloadData()
-    }
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return reservas.count
-    }
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let reserva = reservas[indexPath.row]
-        print("\(reserva.titulo) \(indexPath)")
-    }
-    
+
+  
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReserva", for: indexPath) as! ReservaTableViewCell
-        let reserva = reservas[indexPath.row]
-        cell.update(with: reserva)
-        cell.showsReorderControl = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
         // Configure the cell...
-        cell.showsReorderControl = true
+
         return cell
     }
-    
+    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -71,16 +49,17 @@ class ReservaTableViewController: UITableViewController {
     }
     */
 
-    
+    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            reservas.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } 
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
     }
-    
+    */
 
     /*
     // Override to support rearranging the table view.
