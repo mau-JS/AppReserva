fd08edf0e69a562064e0ffc8ecd7099c41925913
Mauricio Juárez <mau@MacBook-Air-de-Mauricio-4.local>
2022-09-29 14:01:20 -0500

    Se completó el login con passwords cifradas

--- appReserva/Login/LoginViewController.swift
+++ appReserva/Login/LoginViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
+import FirebaseAnalytics
 
 class LoginViewController: UIViewController {
-
-    @IBOutlet var eMail: UITextField!
-    @IBOutlet var login: UIButton!
-    @IBOutlet var Contrasena: UITextField!
+    
+    @IBOutlet var eMailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var LoginButton: UIButton!
     @IBOutlet var errorLabel: UILabel!
-   
     override func viewDidLoad() {
         super.viewDidLoad()
        setUpElements()
@@ -21,11 +23,32 @@ class LoginViewController: UIViewController {
     
     func setUpElements(){
         errorLabel.alpha = 0
-        Utilities.styleTextField(eMail)
-        Utilities.styleTextField(Contrasena)
-        Utilities.styleFilledButton(login)
+        Utilities.styleTextField(eMailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(LoginButton)
     }
     
     @IBAction func presionaLogin(_ sender: Any) {
+        //Validar campos
+        
+        //Limpiando espacios de los text fields y asumiendo que todos los campos son válidos
+        let email = eMailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        //Login al usuario
+        Auth.auth().signIn(withEmail: email, password: password){(result, error) in
+            if error != nil{
+                //No se pudo iniciar sesión. Desplegando tipo de error
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            }
+            else{
+                let homeViewController =
+                self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewcontroller) as? HomeViewController
+                self.view.window?.rootViewController = homeViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+            
+        }
+        
     }
 }
