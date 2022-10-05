e0f9f3d3eab9fcae491559c513425a2f1b7f5570
Mauricio Juárez <mau@MacBook-Air-de-Mauricio-4.local>
2022-10-05 14:02:00 -0500

    Se arregló problema con tab bar y corrigió botones

--- appReserva/Login/MainTabBarController.swift
+++ appReserva/Login/MainTabBarController.swift
@@ -10,7 +10,6 @@ import FirebaseFirestore
 import FirebaseAuth
 import FirebaseAnalytics
 class MainTabBarController: UITabBarController {
-    @IBOutlet var TabBar: UITabBar!
     //Recibiendo información desde pantalla de inicio
     var idUsuario: String = "Hola logré imprimir"
     override func viewDidLoad() {
@@ -20,8 +19,6 @@ class MainTabBarController: UITabBarController {
         
         //print(idUsuario)
         Usuario.id = idUsuario
-        TabBar.tintColor = UIColor.init(red: 204/255, green: 0/255, blue: 204/255, alpha: 0.5)
-        TabBar.unselectedItemTintColor = .lightGray
         //Para enviar información a tab bars sin navigation controller
         
         /*let navController = self.tabBarController!.viewControllers![1] as! UINavigationController
