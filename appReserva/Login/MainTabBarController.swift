182c17f06ff6cdad01c88e901e01559581ed6967
Mauricio Juárez <mau@MacBook-Air-de-Mauricio-4.local>
2022-10-05 13:45:03 -0500

    Diseño de botones

--- appReserva/Login/MainTabBarController.swift
+++ appReserva/Login/MainTabBarController.swift
@@ -10,6 +10,7 @@ import FirebaseFirestore
 import FirebaseAuth
 import FirebaseAnalytics
 class MainTabBarController: UITabBarController {
+    @IBOutlet var TabBar: UITabBar!
     //Recibiendo información desde pantalla de inicio
     var idUsuario: String = "Hola logré imprimir"
     override func viewDidLoad() {
@@ -19,6 +20,8 @@ class MainTabBarController: UITabBarController {
         
         //print(idUsuario)
         Usuario.id = idUsuario
+        TabBar.tintColor = UIColor.init(red: 204/255, green: 0/255, blue: 204/255, alpha: 0.5)
+        TabBar.unselectedItemTintColor = .lightGray
         //Para enviar información a tab bars sin navigation controller
         
         /*let navController = self.tabBarController!.viewControllers![1] as! UINavigationController
