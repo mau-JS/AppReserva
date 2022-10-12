c318dd45708f8d73887a1a2487426b95b2dc32e1
Mauricio Juárez <mau@MacBook-Air-de-Mauricio-4.local>
2022-10-12 15:59:50 -0500

    Se agregó molde de Ajustes

--- appReserva/Ajustes/AjustesTableViewCell.swift
+++ appReserva/Ajustes/AjustesTableViewCell.swift
@@ -6,40 +6,33 @@
 //
 
 import UIKit
-import SkeletonView
 
-class Hub2ViewController: UIViewController, UITableViewDataSource, SkeletonTableViewDataSource {
+
+class Hub2ViewController: UIViewController, UITableViewDataSource {
   
     @IBOutlet var tableView: UITableView!
     var data = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = 120
-        tableView.estimatedRowHeight = 120
+        tableView.rowHeight = 100
+        tableView.estimatedRowHeight = 100
         tableView.dataSource = self
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
-            for _ in 0 ..< 30{
+            for _ in 0 ..< 5 {
                 self.data.append("Some Text")
             }
-            self.tableView.stopSkeletonAnimation()
-            self.view.hideSkeleton(reloadDataAfter: true, transition: .crossDissolve(0.25))
             self.tableView.reloadData()
         })
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        tableView.isSkeletonable = true
-        tableView.showAnimatedGradientSkeleton(usingGradient: .init(baseColor: .link), animation: nil, transition: .crossDissolve(0.25))
-        //tableView.showAnimatedSkeleton(usingColor: .concrete, transition: //.crossDissolve(0.25))
+
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
     
-    func collectionSkeletonView(_ skeletonView: UITableView, cellIdentifierForRowAt indexPath: IndexPath) -> ReusableCellIdentifier {
-        return "MyTableViewCell"
-    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableViewCell", for: indexPath) as! Carta2TableViewCell
         if !data.isEmpty {
