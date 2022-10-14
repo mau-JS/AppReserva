551bb5d4d1802d02fc82a9821b2ee2ff9feaf2d3
Mauricio Juárez <mau@MacBook-Air-de-Mauricio-4.local>
2022-10-14 15:35:19 -0500

    Iniciando programación de galería de Hub

--- appReserva/Hub/firstTabViewController.swift
+++ appReserva/Hub/firstTabViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 import FirebaseCore
 import FirebaseAuth
 import FirebaseDatabase
@@ -14,21 +16,83 @@ import FirebaseAnalytics
 
 
 
-class firstTabViewController: UIViewController {
+class firstTabViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     @IBOutlet var botonSalida: UIButton!
     
+    let imageNames = Array(1...6).map {"image\($0)"}
+    
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.identifier)
+        return collectionView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        UILabel.appearance().substituteFontName = Usuario.fondo // USE YOUR FONT NAME INSTEAD
-        UITextView.appearance().substituteFontName = Usuario.fondo // USE YOUR FONT NAME INSTEAD
-        UITextField.appearance().substituteFontName = Usuario.fondo
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+      
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
     }
     
+   
+    
+    
+    
+    
+   
 
     
     @IBAction func presionaBoton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.identifier, for: indexPath) as? ImageCollectionViewCell else{
+            fatalError()
+        }
+        cell.imageView.image = UIImage(named: imageNames[indexPath.row])
+        return cell
+    }
+    
 }
+
+class ImageCollectionViewCell: UICollectionViewCell{
+    static let identifier = "ImageCollectionViewCell"
+    let imageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    override init (frame: CGRect){
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+}
+
+
